ee894bb81622e044a2c14a8ff260f79db2e1280e
pillepillz <phigul@kth.se>
2016-02-10 14:30:32 +0100

    Update dinnerModel.js

--- js/model/dinnerModel.js
+++ js/model/dinnerModel.js
@@ -4,6 +4,7 @@ var DinnerModel = function() {
 	//TODO Lab 2 implement the data structure that will hold number of guest
 	// and selected dinner options for dinner menu
 	this.guests = 0;
+	var num = 2
 	
 	this.setNumberOfGuests = function(num) {
 		//TODO Lab 2
