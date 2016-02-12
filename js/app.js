ce0a998ed109e67989b3ee8deb955c3d8ac58c40
pillepillz <phigul@kth.se>
2016-02-12 16:00:49 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -5,7 +5,6 @@ $(function() {
     //And create the needed controllers and views
     var exampleView = new ExampleView($("#exampleView"));
  
-    for( i in (model.getSelectedDish("starter"))){
-        console.log((model.getSelectedDish("starter"))[i]);
+    console.log(model.getSelectedDish("starter"));
     }
 })
