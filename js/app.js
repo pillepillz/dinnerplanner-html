66b647cd3c81d70c1f2b7b8c76c88b46cd57b694
pillepillz <phigul@kth.se>
2016-02-12 17:57:15 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,10 +1,17 @@
-$(function() {
+$(function(){
     //We instantiate our model
     var model = new DinnerModel();
  
     //And create the needed controllers and views
     var exampleView = new ExampleView($("#exampleView"));
- 
+
     console.log(model.getSelectedDish("starter"));
-    }
+    model.addDishToMenu(1);
+    model.addDishToMenu(100);
+    console.log(model.getFullMenu());
+    console.log(model.getSelectedDish("starter"));
+    console.log(model.getAllIngredients());
+    console.log(model.getTotalMenuPrice());
+    
+
 })
